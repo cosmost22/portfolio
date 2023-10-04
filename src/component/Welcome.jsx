b4517f6121cd7acea914fa88003d47e63bd75235
Park SangTak <cosmost22@naver.com>
2023-10-04 10:48:07 +0900

    save

---
+++ src/component/Welcome.jsx
@@ -0,0 +1,17 @@
+
+
+const Welcome = () => {
+
+
+    return(
+
+        <div>
+            <h1>반갑습니다.</h1>
+            <h3>이곳은 페이지의 최상단입니다.</h3>
+        </div>
+
+    );
+
+}
+
+export default Welcome;
